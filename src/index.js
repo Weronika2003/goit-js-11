fc85d0bc9bac9f949b30f989ebff10ecf4075be1
Weronika2003 <creative.name2003@gmail.com>
2023-07-18 10:24:02 +0200

    ooo

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import '/src/styles.css';
 import axios from 'axios';
 import Notiflix from 'notiflix';
-import SimpleLightbox from 'Simplelightbox';
+import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 
 const galleryEl = document.querySelector('.gallery');
